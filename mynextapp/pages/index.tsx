e81712e0e7d0319d9eff5e01fce51669916e2d41
yamadamasashi <shisama94@gmail.com>
2022-03-29 09:52:07 +0900

    ディレクトリ構成について缶ゲル

--- mynextapp/pages/index.tsx
+++ mynextapp/pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import PageHead from './Head'
 import { Footer } from '../components/Footer'
 
@@ -9,6 +10,11 @@ const Home: NextPage = () => {
     <>
       <PageHead />
       <Footer sns={snsArray} id={30} />
+
+      <br />
+      <Link href="https://note.com/tabelog_frontend/n/n07b4077f5cf3">
+        <a>ディレクトリ構成についてい考える</a>
+      </Link>
     </>
   )
 }
