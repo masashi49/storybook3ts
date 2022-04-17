137933a7b5ecf800a323091dc97bb5170504c1ad
yamadamasashi <shisama94@gmail.com>
2022-04-18 08:51:19 +0900

    generics

--- mynextapp/components/Generics.tsx
+++ mynextapp/components/Generics.tsx
@@ -1,20 +1,29 @@
 import { VFC } from 'react'
 
-function copy<T>(value: T) {
+function copy<T, U>(value: T, ai: U) {
   return value
 }
-console.log(copy<string>("hello"))
+type Hoge = {
+  aisatu: string;
+}
+const hello = {
+  aisatu: "hello"
+}
 
+console.log(copy<Hoge, string>(hello, "hello"))
+console.log(copy(hello, "hello")) // 型推論してくれる！
 
 
 export const Generics: VFC = () => {
   return (
     <div>
       genericsをなぜ使うのか
-      - 引数をanyとすると、使用する際に型保管が効かない。
-      - 使用する際に、型アサーション(asを使う) で無理やり変換すれば使える。
-      - stringやnumberといったシンプルなものなら大丈夫だが、objectの場合、内包するkeyとvalueの方を全部記載する必要があり、辛い。
-      - 汎用的につい書いたい関数に対し、型も引数として一緒に渡せばさっきまでの辛みが解消される。これを実行できるのがGenericsである。
+      <ul>
+        <li>- 汎用的に使いたい関数に対し、型も引数として一緒に渡せば下記の辛みが解消される。これを実行できるのがGenericsである。</li>
+        <li>- 引数をanyとすると、使用する際に型保管が効かない。</li>
+        <li>- 使用する際に、型アサーション(asを使う) で無理やり変換すれば使える。</li>
+        <li>- stringやnumberといったシンプルなものなら大丈夫だが、objectの場合、内包するkeyとvalueの方を全部記載する必要があり、辛い。</li>
+      </ul>
     </div>
   )
 }
